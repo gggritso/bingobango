59d3223aab89232ee78b839889fe05e31f937cec
George Gritsouk <989898+gggritso@users.noreply.github.com>
2020-04-08 21:34:35 -0400

    Initial commit

---
+++ src/js/components/Board.jsx
@@ -0,0 +1,45 @@
+import React, { Component, Fragment } from "react";
+
+import CARDS from "../data/cards";
+
+export class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    this.daub = this.daub.bind(this);
+
+    this.state = {
+      daubed: [],
+      board: CARDS.test,
+    };
+  }
+
+  daub(square) {
+    this.setState({
+      daubed: [...this.state.daubed, square],
+    });
+  }
+
+  render() {
+    return (
+      <div className="grid grid-cols-5 grid-rows-5">
+        {this.state.board.map((row, y) => (
+          <div className="row" key={y}>
+            {row.map((square, x) => {
+              return (
+                <div
+                  className={`relative`}
+                  style={{ "padding-bottom": "100%" }}
+                  key={x}
+                  onClick={() => this.daub(square)}
+                >
+                  <span>{square}</span>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import "../index.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { Board } from "./components/Board.jsx";
+
+ReactDOM.render(<Board />, document.getElementById("board"));
