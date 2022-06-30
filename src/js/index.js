dde6d7511dfe674a024708e99aa71575885a1ef1
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-06-30 10:22:14 -0400

    Set Sentry release number

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
   dsn: "https://c0fea8d279d1446ba514cdb33036c710@o1301100.ingest.sentry.io/6536661",
   integrations: [new BrowserTracing()],
+  release: "bingobango@0.0.1",
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
